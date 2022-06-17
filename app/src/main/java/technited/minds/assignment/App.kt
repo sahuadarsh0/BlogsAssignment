5fbecffc2b5cd3286dd4f1018915b7cd8ab7a4fb
sahuadarsh0 <sahuadarsh0@gmail.com>
2022-06-17 20:29:52 +0530

    Added Dependencies  - Hilt Retrofit Glide Added Launcher Icons Added Splash Screen

---
+++ app/src/main/java/technited/minds/assignment/App.kt
@@ -0,0 +1,7 @@
+package technited.minds.assignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/technited/minds/assignment/MainActivity.kt
+++ app/src/main/java/technited/minds/assignment/MainActivity.kt
@@ -1,11 +1,22 @@
 package technited.minds.assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dagger.hilt.android.AndroidEntryPoint
+import technited.minds.assignment.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        setTheme(R.style.Theme_BlogsAssignment)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
